84a05d932e42a5b978baabfce7181e8b23825fef
Stephen McCosker <Stephen.McCosker@agriculture.gov.ie>
2019-09-13 16:54:57 +0100

    Initial commit

---
+++ src/main/java/ie/gov/agriculture/fisheries/la/capacityservice/entity/AllCapacity.java
@@ -0,0 +1,10 @@
+package ie.gov.agriculture.fisheries.la.capacityservice.entity;
+
+import java.util.List;
+
+public class AllCapacity {
+	private String ownerId;
+	
+	private List<Capacity> onRegister;
+	private List<Capacity> offRegister;
+}
