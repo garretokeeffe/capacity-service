b6858ea86001a92f076151687a69bfe060c90c98
Stephen McCosker <Stephen.McCosker@agriculture.gov.ie>
2019-09-13 16:53:30 +0100

    Latest updates.

--- src/main/java/ie/gov/agriculture/fisheries/la/capacityservice/controller/CustomerCapacityController.java
+++ src/main/java/ie/gov/agriculture/fisheries/la/capacityservice/controller/CustomerCapacityController.java
@@ -10,21 +10,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ie.gov.agriculture.fisheries.la.capacityservice.dto.AllCapacityDTO;
 import ie.gov.agriculture.fisheries.la.capacityservice.dto.CustomerCapacityDTO;
 import ie.gov.agriculture.fisheries.la.capacityservice.service.CustomerCapacityService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
-@RequestMapping("sfos/customer/capacity")
+@RequestMapping("sfos")
 @Api(value = "customerCapacityServices")
 public class CustomerCapacityController {
 	@Autowired
 	CustomerCapacityService customerCapacityService;
+	
+	@ApiOperation(value = "Retrieve complete AllCapacity information for an individual customer [e.g. http://localhost:8080/sfos/capacity/ccs/23630]")
+	@GetMapping(path = "/capacity/ccs/{customerId}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<AllCapacityDTO> getCustomerCapacity(@PathVariable(required = true) String customerId) throws Exception {
+		AllCapacityDTO allCapacityDTO = customerCapacityService.getAllCapacity(customerId);
+		
+		return new ResponseEntity<AllCapacityDTO>(allCapacityDTO, HttpStatus.OK);
+	}
 
 	@ApiOperation(value = "Retrieve capacity information for an individual customer [e.g. http://localhost:8080/sfos/customer/capacity/23630]")
-	@GetMapping(path = "/{customerId}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<CustomerCapacityDTO>> getVesselDetailsyId(@PathVariable(required = true) String customerId) throws Exception {
+	@GetMapping(path = "/customer/capacity/{customerId}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<CustomerCapacityDTO>> getCustomerCapacityInformation(@PathVariable(required = true) String customerId) throws Exception {
 		List<CustomerCapacityDTO> customerCapacityDTO = customerCapacityService.getCustomerCapacityInformation(customerId);
 		
 		return new ResponseEntity<>(customerCapacityDTO, HttpStatus.OK);
