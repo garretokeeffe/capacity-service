975f693e7c1de3c2a5c245bd0417810a33702a33
Stephen McCosker <Stephen.McCosker@agriculture.gov.ie>
2019-09-16 12:47:04 +0100

    Latest.

--- src/main/java/ie/gov/agriculture/fisheries/la/capacityservice/repository/CapacityRepository.java
+++ src/main/java/ie/gov/agriculture/fisheries/la/capacityservice/repository/CapacityRepository.java
@@ -15,8 +15,8 @@ public interface CapacityRepository extends CrudRepository<Capacity, String> {
 	@Query (
 		value = "" +
 			"SELECT A.ID, A.capaccountid, A.capsegmentid, A.ownerid, A.offregister, A.fleetSegment, A.fleetSubSegment, " + 
-			"COALESCE((SELECT B.GROSSBALANCE FROM capaccountsegment B WHERE B.capaccountid = A.capaccountid AND B.UOMID = 91359 and B.segmentid = A.segmentid), 0) gt, " + 
-			"COALESCE(A.kw, 0) kw, A.vesselid, A.proposed /*, A.segmentid, A.CREATEDATE*/ FROM ( " + 
+			"COALESCE((SELECT case when a.statusid = 6237299 /* IF PROPOSED */ then B.PROPOSEDBALANCE else B.GROSSBALANCE end FROM capaccountsegment B WHERE B.capaccountid = A.capaccountid AND B.UOMID = 91359 and B.segmentid = A.segmentid), 0) gt, " +  
+			"COALESCE(A.kw, 0) kw, A.vesselid, A.vesselname, case when A.offregister='true' then A.proposed else null end proposed FROM ( " + 
 			"select rownum ID, a.capaccountid, b.capsegmentid, a.ownerid, " + 
 			"case when (a.statusid = 6237297 and c.STATUSID = 1811007) then 'false' else 'true' end offRegister, " + 
 			"case" + 
@@ -27,16 +27,16 @@ public interface CapacityRepository extends CrudRepository<Capacity, String> {
 			"    when (b.segmentid in (1942696)) then '5' " + 
 			"    else '0' " + 
 			"end fleetSegment, " + 
-			"case when (b.segmentid is null) then 0 else b.segmentid end fleetSubSegment, a.vesselid, " + 
+			"case when (b.segmentid is null) then 0 else b.segmentid end fleetSubSegment, a.vesselid, c.vesselname, " + 
 			"case when (a.statusid = 6237299) then 'true' else 'false' end proposed, b.segmentid, " + 
-			"(select GROSSBALANCE from capaccountsegment where CAPACCOUNTID = a.capaccountid and CAPSEGMENTID = b.capsegmentid and UOMID = 91359) gt, " + 
-			"(select GROSSBALANCE from capaccountsegment where CAPACCOUNTID = a.capaccountid and CAPSEGMENTID = b.capsegmentid and UOMID = 91290) kw, " + 
-			"a.CREATEDATE, ROW_NUMBER() OVER (partition by a.capaccountid ORDER BY a.capaccountid) CapAccRank " + 
+			"/*(select GROSSBALANCE from capaccountsegment where CAPACCOUNTID = a.capaccountid and CAPSEGMENTID = b.capsegmentid and UOMID = 91359) gt,*/ " + 
+			"(select case when a.statusid = 6237299 /* IF PROPOSED */ then B.PROPOSEDBALANCE else B.GROSSBALANCE end from capaccountsegment where CAPACCOUNTID = a.capaccountid and CAPSEGMENTID = b.capsegmentid and UOMID = 91290) kw, " +  
+			"a.CREATEDATE, ROW_NUMBER() OVER (partition by a.capaccountid ORDER BY a.capaccountid) CapAccRank, a.statusid " + 
 			"from capacityaccount a " + 
 			"inner join capaccountsegment b on b.CAPACCOUNTID = a.CAPACCOUNTID " + 
 			"inner join vessel c on a.vesselid = c.vesselid " + 
 			"where ownerid = :ownerId and a.inactiveind = 'N' and b.inactiveind = 'N' /*and a.capaccountid = 23906141*/ " + 
-			") A WHERE A.CapAccRank = 2 order by A.CREATEDATE", nativeQuery = true
+			") A WHERE A.CapAccRank = 2 order by A.offregister, A.proposed, A.CREATEDATE", nativeQuery = true
 		)
 		public List<Capacity> findCapacityByOwnerId (@Param("ownerId") String ownerId);
 }
