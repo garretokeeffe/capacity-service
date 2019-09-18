2d89594cd3c6adfe01b679253bebe1e6569f3506
Stephen McCosker <Stephen.McCosker@agriculture.gov.ie>
2019-09-18 14:16:24 +0100

    Latest updates.

--- src/main/java/ie/gov/agriculture/fisheries/la/capacityservice/entity/PenaltyPoints.java
+++ src/main/java/ie/gov/agriculture/fisheries/la/capacityservice/entity/PenaltyPoints.java
@@ -1,56 +1,32 @@
 package ie.gov.agriculture.fisheries.la.capacityservice.entity;
 
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-/**
- * The penalty points entity
- * @author garret.okeeffe
- *
- */
 @Entity
 @Getter @Setter @NoArgsConstructor @ToString
-public class PenaltyPoints {
-
+public class PenaltyPoints {	
 	@Id
+	@Column(name = "ID") //TDFI_CAPACITYACCOUNTPOINTSID
+	private Long ID;
+	
+	@Column(name = "ASSIGNEDPOINTS")
+	private String ASSIGNEDPOINTS;
+	
+	@Column(name = "EXPIRYDATE")
+	private String EXPIRYDATE;
+	
+	@Column(name = "PENALTYCOMMENT")
+	private Long PENALTYCOMMENT;
+
 	@Column(name = "CAPACCOUNTID")
-	private Long id;
+	private String CAPACCOUNTID;
 	
-	@Column(name = "OWNERID")
-	private String ownerId;
-	
-	@Column(name = "CUSTOMERNAME")
-	private String customerName;
-	
-	@Column(name = "SEGMENTID")
-	private Long segmentId;
-	
-	@Column(name = "CROSSSEGMENT")
-	private String crossSegment;
-	
-	@Column(name = "ASSIGNEDGT")
-	private Float assignedGT;
-	@Column(name = "PENDINGGT")
-	private Float pendingGT;
-	
-	@Column(name = "ASSIGNEDKW")
-	private Float assignedKW;
-	@Column(name = "PENDINGKW")
-	private Float pendingKW;
-	
-	@Column(name = "ASSIGNPOINTS")
-	private Float pointsAssigned;
-	
-	@Column(name = "POINTSEXPIREDATE")
-	private Date expiryDate;
-	
-	public transient String reasons;
+	@Column(name = "CAPSEGMENTID")
+	private String CAPSEGMENTID;
 }
