1cc722c9034f926a30990bb8b45fbd4c435d4edd
Stephen McCosker <Stephen.McCosker@agriculture.gov.ie>
2019-09-19 10:27:33 +0100

    Code clean-up.

--- src/main/java/ie/gov/agriculture/fisheries/la/capacityservice/controller/CustomerCapacityController.java
+++ src/main/java/ie/gov/agriculture/fisheries/la/capacityservice/controller/CustomerCapacityController.java
@@ -1,7 +1,6 @@
 package ie.gov.agriculture.fisheries.la.capacityservice.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import ie.gov.agriculture.fisheries.la.capacityservice.dto.AllCapacityDTO;
 import ie.gov.agriculture.fisheries.la.capacityservice.dto.CustomerCapacityDTO;
 import ie.gov.agriculture.fisheries.la.capacityservice.service.CustomerCapacityService;
