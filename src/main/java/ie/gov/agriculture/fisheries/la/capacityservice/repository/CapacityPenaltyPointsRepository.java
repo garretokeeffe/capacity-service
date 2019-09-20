575f9dbaa57962af42a2ec153909823f52cf3729
Stephen McCosker <Stephen.McCosker@agriculture.gov.ie>
2019-09-20 16:15:54 +0100

    Added CompletableFuture class for Async calls.

--- src/main/java/ie/gov/agriculture/fisheries/la/capacityservice/repository/CapacityPenaltyPointsRepository.java
+++ src/main/java/ie/gov/agriculture/fisheries/la/capacityservice/repository/CapacityPenaltyPointsRepository.java
@@ -1,9 +1,11 @@
 package ie.gov.agriculture.fisheries.la.capacityservice.repository;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import ie.gov.agriculture.fisheries.la.capacityservice.entity.PenaltyPoints;
@@ -19,5 +21,6 @@ public interface CapacityPenaltyPointsRepository extends CrudRepository<PenaltyP
 			"left outer join TDFI_PENALTYPOINTS c on b.TDFI_PENALTYPOINTSID = c.TDFI_PENALTYPOINTSID " + 
 			"where a.CAPACCOUNTID = :capAccountId and a.inactiveind = 'N' and b.inactiveind = 'N' order by a.CAPACCOUNTID, b.CAPSEGMENTID", nativeQuery = true
 	)
-	public List<PenaltyPoints> findCustomerCapacityPenaltyPointsByCapAccountId (@Param("capAccountId") Integer capAccountId);
+	@Async
+	public CompletableFuture<List<PenaltyPoints>> findCustomerCapacityPenaltyPointsByCapAccountId (@Param("capAccountId") Integer capAccountId);
 }
