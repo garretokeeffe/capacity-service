493d65f9fe5d55b6837b27a383797edb58ad658e
Stephen McCosker <Stephen.McCosker@agriculture.gov.ie>
2019-09-23 16:55:46 +0100

    Improvements to query.

--- src/main/java/ie/gov/agriculture/fisheries/la/capacityservice/repository/CapacityRepository.java
+++ src/main/java/ie/gov/agriculture/fisheries/la/capacityservice/repository/CapacityRepository.java
@@ -13,32 +13,32 @@ import ie.gov.agriculture.fisheries.la.capacityservice.entity.Capacity;
 public interface CapacityRepository extends CrudRepository<Capacity, String> {
 	@Query (
 		value = "" +
-			"SELECT A.ID, A.capaccountid, /*A.capsegmentid,*/ A.ownerid, A.offregister, A.fleetSegment, A.fleetSubSegment, " + 
-			"COALESCE((SELECT case when a.statusid = 6237299 /* IF PROPOSED */ then B.PROPOSEDBALANCE else B.GROSSBALANCE end FROM capaccountsegment B WHERE B.capaccountid = A.capaccountid AND B.UOMID = 91359 and B.segmentid = A.segmentid), 0) gt, " +  
-			"COALESCE(A.kw, 0) kw, A.vesselid, A.vesselname, case when A.offregister='true' then A.proposed else null end proposed FROM ( " + 
-			"select rownum ID, a.capaccountid, b.capsegmentid, a.ownerid, " + 
-			"case when (a.statusid = 6237297 and c.STATUSID = 1811007) then 'false' else 'true' end offRegister, " + 
-			"case" + 
-			"    when (b.segmentid in (1942697, 338360921, 1942700, 338360917)) then '1' " + 
-			"    when (b.segmentid in (1807585)) then '2' " + 
-			"    when (b.segmentid in (1942695, 338360923)) then '3' " + 
-			"    when (b.segmentid in (1942701)) then '4' " + 
-			"    when (b.segmentid in (1942696)) then '5' " + 
-			"    else '0' " + 
-			"end fleetSegment, " + 
-			"case when (b.segmentid is null) then 0 else b.segmentid end fleetSubSegment, a.vesselid, c.vesselname, " + 
-			"case when (a.statusid = 6237299) then 'true' else 'false' end proposed, b.segmentid, " + 
-			"/*(select GROSSBALANCE from capaccountsegment where CAPACCOUNTID = a.capaccountid and CAPSEGMENTID = b.capsegmentid and UOMID = 91359) gt,*/ " + 
-			//"--(select case when a.statusid = 6237299 /* IF PROPOSED */ then B.PROPOSEDBALANCE else B.GROSSBALANCE end from capaccountsegment B where CAPACCOUNTID = a.capaccountid and CAPSEGMENTID = b.capsegmentid and UOMID = 91290) kw, " +  
-			"(select case when a.statusid = 6237299 /* IF PROPOSED */ then D.PROPOSEDBALANCE else D.GROSSBALANCE end from capaccountsegment D where D.CAPACCOUNTID = a.capaccountid and D.CAPSEGMENTID = b.capsegmentid and D.SEGMENTID = b.segmentid and D.UOMID = 91290) kw, " +
-			"a.CREATEDATE, ROW_NUMBER() OVER (partition by a.capaccountid ORDER BY a.capaccountid, case when a.statusid = 6237299 /* IF PROPOSED */ then b.PROPOSEDBALANCE else b.GROSSBALANCE end) CapAccRank, a.statusid " + 
-			"from capacityaccount a " + 
-			"inner join capaccountsegment b on b.CAPACCOUNTID = a.CAPACCOUNTID " + 
-			"inner join vessel c on a.vesselid = c.vesselid " + 
-			"where ownerid = :ownerId and a.inactiveind = 'N' and b.inactiveind = 'N' and c.inactiveind = 'N' " + 
-			") A WHERE A.CapAccRank = 2 " +
-			"and (kw > 0 or (SELECT case when a.statusid = 6237299 then MAX(B.PROPOSEDBALANCE) else MAX(B.GROSSBALANCE) end FROM capaccountsegment B WHERE B.capaccountid = A.capaccountid AND B.UOMID = 91359 and B.segmentid = A.segmentid ) > 0) " +
-			"order by A.offregister, A.proposed, A.CREATEDATE", nativeQuery = true
+			"Select rownum ID, A.capaccountid, A.ownerid, A.offRegister, A.fleetSegment, A.fleetSubSegment, DECODE(A.proposed, 0, A.GT, A.GT_Proposed) GT, DECODE(A.proposed, 0, A.KW, A.KW_Proposed) KW, " +  
+			"    A.VESSELID, A.vesselname, DECODE(A.proposed, 1, 'true', 'false') proposed FROM ( " +  
+			"    SELECT ca.capaccountid, ca.ownerid, cas.segmentid fleetSubSegment, " + 
+			"    SUM(DECODE(cas.uomid, '91359', cas.grossbalance, 0)) GT, " + 
+			"    SUM(DECODE(cas.uomid, '91290', cas.grossbalance, 0)) KW, " +  
+			"    SUM(DECODE(cas.uomid, '91359', cas.proposedbalance, 0)) GT_Proposed, " +  
+			"    SUM(DECODE(cas.uomid, '91290', cas.proposedbalance, 0)) KW_Proposed, " + 
+			"    ca.VESSELID, " + 
+			"    max(case when (ca.statusid = 6237297 and v.STATUSID = 1811007) then 'false' else 'true' end) offRegister, " + 
+			"    max(case  " +  
+			"        when (cas.segmentid in (1942697, 338360921, 1942700, 338360917)) then '1' " +  
+			"        when (cas.segmentid in (1807585)) then '2' " +  
+			"        when (cas.segmentid in (1942695, 338360923)) then '3' " +  
+			"        when (cas.segmentid in (1942701)) then '4' " +  
+			"        when (cas.segmentid in (1942696)) then '5' " +  
+			"        else '0' " +  
+			"    end) fleetSegment, max(v.vesselname) vesselname, " +  
+			"    DECODE(ca.statusid, 6237299, 1, 0) proposed, max(ca.statusid) statusid " +  
+			"    FROM CAPACITYACCOUNT ca, CAPACCOUNTSEGMENT cas, vessel v " +  
+			"    WHERE ca.ownerid = :ownerId AND ca.capaccountid = cas.capaccountid " +  
+			"    and ca.vesselid = v.vesselid AND ca.STATUSID in (6237298, 6237297, 6237299) " +  
+			"    AND cas.inactiveind = 'N' AND ca.inactiveind = 'N' AND ca.closedind = 'N' AND (v.inactiveind = 'N' and v.statusid is not null) " +  
+			"    HAVING (SUM(DECODE(cas.uomid, '91359', cas.grossbalance, 0)) > 0) OR (SUM(DECODE(cas.uomid, '91290', cas.grossbalance, 0)) > 0) " +  
+			"       OR (SUM(DECODE(cas.uomid, '91290', cas.proposedbalance, 0)) > 0) OR (SUM(DECODE(cas.uomid, '91290', cas.proposedbalance, 0)) > 0) " +  
+			"    GROUP BY ca.capaccountid, ca.ownerid, cas.segmentid, ca.STATUSID, ca.VESSELID " +  
+			") A order by A.offregister, A.statusid, A.proposed, A.vesselname", nativeQuery = true
 	)
 	public List<Capacity> findCapacityByOwnerId (@Param("ownerId") String ownerId);
 }
