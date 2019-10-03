b6137e6ab8c137aece5de30aa023959d6182cab0
Stephen McCosker <Stephen.McCosker@agriculture.gov.ie>
2019-10-03 10:08:58 +0100

    Minor updates.

--- src/main/java/ie/gov/agriculture/fisheries/la/capacityservice/repository/CapacityRepository.java
+++ src/main/java/ie/gov/agriculture/fisheries/la/capacityservice/repository/CapacityRepository.java
@@ -14,7 +14,7 @@ public interface CapacityRepository extends CrudRepository<Capacity, String> {
 	@Query (
 		value = "" +
 			"Select rownum ID, A.capaccountid, A.ownerid, A.offRegister, A.fleetSegment, A.fleetSubSegment, DECODE(A.proposed, 0, A.GT, A.GT_Proposed) GT, DECODE(A.proposed, 0, A.KW, A.KW_Proposed) KW, " +  
-			"    A.VESSELID, A.vesselname, DECODE(A.proposed, 1, 'true', 'false') proposed FROM ( " +  
+			"    A.VESSELID, A.vesselname, case when A.offRegister='false' then null else DECODE(A.proposed, 1, 'true', 'false') end proposed FROM ( " +  
 			"    SELECT ca.capaccountid, ca.ownerid, cas.segmentid fleetSubSegment, " + 
 			"    SUM(DECODE(cas.uomid, '91359', cas.grossbalance, 0)) GT, " + 
 			"    SUM(DECODE(cas.uomid, '91290', cas.grossbalance, 0)) KW, " +  
@@ -33,11 +33,11 @@ public interface CapacityRepository extends CrudRepository<Capacity, String> {
 			"    DECODE(ca.statusid, 6237299, 1, 0) proposed, max(ca.statusid) statusid " +  
 			"    FROM CAPACITYACCOUNT ca, CAPACCOUNTSEGMENT cas, vessel v " +  
 			"    WHERE ca.ownerid = :ownerId AND ca.capaccountid = cas.capaccountid " +  
-			"    and ca.vesselid = v.vesselid AND ca.STATUSID in (6237298, 6237297, 6237299) " +  
-			"    AND cas.inactiveind = 'N' AND ca.inactiveind = 'N' AND ca.closedind = 'N' AND (v.inactiveind = 'N' and v.statusid is not null) " +
+			"    and ca.vesselid = v.vesselid AND ca.STATUSID in (6237298, 6237297, 6237299) /*Assigned, Unassigned, Proposed*/  " +  
+			"    AND cas.inactiveind = 'N' AND ca.inactiveind = 'N' AND ca.closedind = 'N' AND (v.inactiveind = 'N' and v.statusid is not null /*Only active vessels with a valid status*/)  " +
 			"    AND CAST(COALESCE(ADD_MONTHS(OFFREGDATE,24),Current_Timestamp+1) AS DATE) > CAST(Current_Timestamp AS DATE) /* Preclude expired capacity */ " +  
-			"    HAVING (SUM(DECODE(cas.uomid, '91359', cas.grossbalance, 0)) > 0) OR (SUM(DECODE(cas.uomid, '91290', cas.grossbalance, 0)) > 0) " +  
-			"       OR (SUM(DECODE(cas.uomid, '91290', cas.proposedbalance, 0)) > 0) OR (SUM(DECODE(cas.uomid, '91290', cas.proposedbalance, 0)) > 0) " +  
+			"    HAVING (SUM(DECODE(cas.uomid, '91359' /*GT*/, cas.grossbalance, 0)) > 0) OR (SUM(DECODE(cas.uomid, '91290' /*KW*/, cas.grossbalance, 0)) > 0) " +  
+			"       OR (SUM(DECODE(cas.uomid, '91359' /*GT*/, cas.proposedbalance, 0)) > 0) OR (SUM(DECODE(cas.uomid, '91290' /*KW*/, cas.proposedbalance, 0)) > 0) " +  
 			"    GROUP BY ca.capaccountid, ca.ownerid, cas.segmentid, ca.STATUSID, ca.VESSELID " +  
 			") A order by A.offregister, A.statusid, A.proposed, A.vesselname", nativeQuery = true
 	)
