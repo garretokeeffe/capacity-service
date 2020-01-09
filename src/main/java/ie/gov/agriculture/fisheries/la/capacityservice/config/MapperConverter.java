4677a70b0000fc4a349fcabc778115a78f6ef938
Garret O'Keeffe <garret.okeeffe@agriculture.gov.ie>
2020-01-09 17:18:28 +0000

    add time conversion for ISO8601

--- src/main/java/ie/gov/agriculture/fisheries/la/capacityservice/config/MapperConverter.java
+++ src/main/java/ie/gov/agriculture/fisheries/la/capacityservice/config/MapperConverter.java
@@ -1,5 +1,7 @@
 package ie.gov.agriculture.fisheries.la.capacityservice.config;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -15,6 +17,8 @@ import org.modelmapper.Converter;
  *
  */
 public class MapperConverter {
+	static final ZoneId id = ZoneId.systemDefault();
+	
 	public static Converter<Date, String> getDateMapper() {
 		return new AbstractConverter<Date, String>() {
 			@Override
@@ -23,21 +27,47 @@ public class MapperConverter {
 			}
 		};
 	}
+	
+	
+	public static Converter<LocalDate, String> geLocaltDateMapper() {
+		return new AbstractConverter<LocalDate, String>() {
+			@Override
+			protected String convert(LocalDate source) {
+				return getDateIso8601(source);
+			}
+		};
+	}
+
 
 	/**
 	 * Method returns the date in ISO8601 format e.g. 2020-01-08T12:37:57.921Z
 	 * 
 	 * @param date - the date to be converted. If its an SQL date, the (lack of) time component is handled.
-	 * @return ISO8601 or an empty string if null date was passed.
+	 * @return ISO8601.
 	 */
 	public static String getDateIso8601(Date date) {
-		final ZoneId id = ZoneId.systemDefault();
-
 		if (date instanceof java.sql.Date) { // sql.Date has no time component
 			return ZonedDateTime.of(((java.sql.Date) date).toLocalDate().atStartOfDay(), id).format(DateTimeFormatter.ISO_INSTANT);
 		} 
 		else {
 			return ZonedDateTime.ofInstant(date.toInstant(), id).format(DateTimeFormatter.ISO_INSTANT);
 		}
+	}
+	
+	
+	/**
+	 * Method returns the date in ISO8601 format e.g. 2020-01-08T12:37:57.921Z
+	 * 
+	 * Note: requirement is to always return the full date-time format with TZ. LocalDate does not have a time component so this is added and also the TZ/ 
+	 * 
+	 * @param date - the date to be converted.
+	 * @return ISO8601.
+	 */
+	public static String getDateIso8601(LocalDate date) {
+		if (date == null) {
+			return null;
+		}
+		LocalDateTime localDateTime = date.atStartOfDay();
+		return ZonedDateTime.of(localDateTime, id).format(DateTimeFormatter.ISO_INSTANT);		
 	}
 }
